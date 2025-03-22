899814cec885b5c1461c588df62ee078b467cf61
fabiogrisard <fabiogrisard@gmail.com>
2025-03-22 02:34:43 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const cacheName = 'exatalaudos-cache-v1';
+const precacheResources = [
+  'index.html',
+  'https://raw.githubusercontent.com/fabiogrisard/exata/refs/heads/main/icon-192x192.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
